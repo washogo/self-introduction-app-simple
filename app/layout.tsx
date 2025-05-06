ab52d1af9987c369e041d05b01f69db99841e1e8
Shogo Wada <aiz4338150@gmail.com>
2025-05-06 13:29:17 +0900

    fix: バージョン修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,18 +9,9 @@ export const metadata: Metadata = {
   description: '山田太郎の自己紹介ページです。',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
